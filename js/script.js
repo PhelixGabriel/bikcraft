4bfddf14c7dcb113c94647f4707d64f8cf1bebb8
Gabriel Félix <gabrielts_96@hotmail.com>
2022-12-26 16:41:10 -0300

    New JavaScript Lessons. First time using Plugins

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar links do Menu
+
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -9,6 +11,69 @@ function ativarLink(link) {
     }
 }
 
-
-
 links.forEach(ativarLink);
+
+
+// Ativar links do orçamento
+
+const parametros = new URLSearchParams (location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked = true
+    }
+}
+
+parametros.forEach(ativarProduto);
+
+// perguntas frequentes
+
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls')
+    const resposta = document.getElementById(controls)
+
+
+    resposta.classList.toggle('ativo')
+    const ativa = resposta.classList.contains('ativo')
+    console.log(ativa)
+    pergunta.setAttribute('aria-expanded', ativa)
+    
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta)
+    console.log(pergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches
+    if(media) {
+      galeriaContainer.prepend(img);
+    }
+    
+    
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//Animação
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
